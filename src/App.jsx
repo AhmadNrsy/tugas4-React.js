646a6f6ab61f58088c030efb2d511345fcae1a80
Ahmad Nurdiansyah <ahmadyt024@gmail.com>
2026-03-09 11:03:29 +0700

    UI update - Ahmad

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
   return (
-    <div>
-      <h1>Tugas Web React</h1>
-      <p>Nama: Ahmad Nurdiansyah</p>
-      <p>NIM: (12450111761)</p>
-      <p>Teknik Informatika Semester 4</p>
+    <div className="container">
+
+      <div className="card">
+
+        <img
+          src="https://i.pravatar.cc/150"
+          alt="profile"
+          className="profile"
+        />
+
+        <h1>Tugas Web React</h1>
+
+        <p>Nama : Ahmad Nurdiansyah</p>
+        <p>NIM : 12450111761</p>
+        <p>Teknik Informatika Semester 4</p>
+
+        <button onClick={() => setCount(count + 1)}>
+          Klik Saya 👍 {count}
+        </button>
+
+      </div>
+
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
