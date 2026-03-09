d9d3d195aef7dbc327829813397c94ed46086098
Ahmad Nurdiansyah <ahmadyt024@gmail.com>
2026-03-09 10:26:25 +0700

    Tugas 4 untuk React Website

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Tugas Web React</h1>
+      <p>Nama: Ahmad Nurdiansyah</p>
+      <p>NIM: (12450111761)</p>
+      <p>Teknik Informatika Semester 4</p>
+    </div>
+  )
+}
+
+export default App
